34e0895593277cde3d4c9f03691c747cf0e8068e
学生 <root@dsnbc.com>
2022-10-26 15:46:25 +0800

    initing...

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="indexBoxClass">
+		<view class="test">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.indexBoxClass{
+		width:100vw;
+		height:100vh;
+		background:linen;
+		.test{
+			background-color: red;
+			width:100%;
+			height:var(--status-bar-height);
+		}
+	}
+	
+</style>
